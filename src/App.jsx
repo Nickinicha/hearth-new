bad6272c6fd159bb16cd8d11a05b364af8cd4b5a
Nickinicha <napraphat_82@hotmail.com>
2026-04-11 20:42:54 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: '#1a1a2e',
+        color: 'white',
+        fontSize: 48,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      Hearth
+    </div>
+  )
+}
